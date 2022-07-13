bb0eb70fc08ed16ed1cc8ea1f3e7ab4056107bb5
Kutsalaba <kutsalaba.nazariy@gmail.com>
2022-07-13 18:34:24 +0300

    Refactoring

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
 
 import '../api/response_data.dart';
 import '../models/playlist_model.dart';
